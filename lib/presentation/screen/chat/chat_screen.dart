a129c3f10fd907b564f1fa7e42299d76d5dc496b
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2024-04-01 23:53:34 -0500

    testformfield para escribir en el chat

--- lib/presentation/screen/chat/chat_screen.dart
+++ lib/presentation/screen/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:flutter_yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:flutter_yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -40,7 +41,8 @@ class _ChatView extends StatelessWidget {
                 },
               ),
             ),
-            Text("hola"),
+            //caja de texto
+            const MessageFieldBox(),
           ],
         ),
       ),
---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(20),
+    );
+
+    final inputDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {},
+      ),
+    );
+    return TextFormField(
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print("tre");
+      },
+      onChanged: (value) {
+        print("ted");
+      },
+    );
+  }
+}
