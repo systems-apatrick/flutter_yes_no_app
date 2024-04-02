760960049358622b97f7713f8cf28fb974172c57
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2024-04-01 22:31:43 -0500

    estilo global de la aplicacion

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # flutter_yes_no_app
 
-Aplicacion de un chat para preguntas de si o no
+Aplicacion de un chat para preguntas de si o no con un theme
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF49149F);
+const List<Color> _colorTheme = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0 && selectedColor < _colorTheme.length,
+            "Color debe ser entre cero y ${_colorTheme.length}");
+
+  ThemeData theme() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _colorTheme[selectedColor],
+      brightness: Brightness.dark,
+    );
+  }
+}
